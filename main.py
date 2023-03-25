f8071502ab17bfca190d485476cab7efc43a7fce
FanderV <113282846+FanderV@users.noreply.github.com>
2023-03-25 08:03:27 +0300

    password

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import hashlib
 import telebot
 
-bot = telebot.TeleBot("5991855005:AAEWyzlP-Kih9JugPZZsiOPm0m1EzPEF2Zs")
+bot = telebot.TeleBot("5989044980:AAFXuN6EZcNDGhSKNgrNuPgdEVqp76yoUTs")
 
 with open('password.txt', 'r') as file:
     password_hash = file.read().strip()
@@ -14,16 +14,15 @@ def plus(stroka):
 
 def logpass(password):
     with open('password.txt', 'r') as file:
-        correct_password = file.read().strip()
-    hp = hashlib.sha256(bytes(password, encoding ='UTF-8'))
+        correct_pass = file.read().strip()
+    hp = hashlib.sha256(bytes(password, encoding='UTF-8'))
     print("Введенный пароль:", password)
     print("Хеш введенного пароля:", hp.hexdigest())
-    print("Хеш правильного пароля:", correct_password)
-    if str(hp.hexdigest()) == str(correct_password):
+    print("Хеш правильного пароля:", correct_pass)
+    if str(hp.hexdigest()) == str(correct_pass):
         return "верно"
     else:
         return "не верно"
-
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -38,7 +37,13 @@ def send_welcome2(message):
 
 
 def get_password(message):
-    bot.reply_to(message, logpass(message.text))
+    password = message.text
+    result = logpass(password)
+    if result == "верно":
+        bot.reply_to(message, "Пароль верный!")
+    else:
+        mes = bot.send_message(message.chat.id, text='Неверный пароль, попробуйте еще раз')
+        bot.register_next_step_handler(mes, get_password)
 
 
 bot.infinity_polling()
